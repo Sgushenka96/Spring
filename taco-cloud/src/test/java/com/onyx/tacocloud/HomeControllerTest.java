56c5f86619db435348894225add564b8abbbe838
sgushenka96 <2013elisey96@gmail.com>
2023-03-16 01:34:32 +0500

    Add HomeControllerTest, but not working - 404

---
+++ taco-cloud/src/test/java/com/onyx/tacocloud/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package com.onyx.tacocloud;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)       // Тест для HomeController
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;            // Внедрение MockMvc
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))                           // Выполнить запрос GET /
+                .andExpect(status().isOk())                           // Ожидается код ответа HTTP 200
+                .andExpect(view().name("home"))        // Ожидается имя представления home
+                .andExpect(content().string(containsString("Welcome to...")));      // Ожидается наличие строк "Welcome to..."
+    }
+}
