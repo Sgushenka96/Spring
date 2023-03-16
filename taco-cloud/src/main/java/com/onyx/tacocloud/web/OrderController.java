be61078975511092d794d4ed8542ec3a8d2680f1
sgushenka96 <2013elisey96@gmail.com>
2023-03-16 18:03:36 +0500

    Add POST

--- taco-cloud/src/main/java/com/onyx/tacocloud/web/OrderController.java
+++ taco-cloud/src/main/java/com/onyx/tacocloud/web/OrderController.java
@@ -1,2 +1,20 @@
-package com.onyx.tacocloud.web;public class OrderController {
+package com.onyx.tacocloud.web;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Slf4j
+@Controller
+@RequestMapping("orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
 }
