aebec4cc77250e68d66d9d6832756912e9ee007f
sgushenka96 <2013elisey96@gmail.com>
2023-03-21 18:41:45 +0500

    JDBC complete

--- taco-cloud/src/main/java/com/onyx/tacocloud/web/OrderController.java
+++ taco-cloud/src/main/java/com/onyx/tacocloud/web/OrderController.java
@@ -2,8 +2,8 @@ package com.onyx.tacocloud.web;
 
 
 import com.onyx.tacocloud.TacoOrder;
+import com.onyx.tacocloud.data.OrderRepository;
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
-@Slf4j
+
 @Controller
 @RequestMapping("orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
@@ -28,8 +33,7 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
