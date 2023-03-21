78e642bdd0ac45bfe442f920637ab8f7778916fd
sgushenka96 <2013elisey96@gmail.com>
2023-03-21 18:24:59 +0500

    JdbcOrder Repository added

--- taco-cloud/src/main/java/com/onyx/tacocloud/Taco.java
+++ taco-cloud/src/main/java/com/onyx/tacocloud/Taco.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,5 +20,5 @@ public class Taco {
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientRef> ingredients;
 }
